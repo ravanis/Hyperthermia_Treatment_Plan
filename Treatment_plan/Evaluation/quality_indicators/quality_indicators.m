98f2e4f79154c0b868b2f7bcf2849d18b74d9903
Andrea Nygren <Andrea Nygren>
2017-07-20 16:34:40 +0200

    Comments and information

--- Treatment_plan/Evaluation/quality_indicators/quality_indicators.m
+++ Treatment_plan/Evaluation/quality_indicators/quality_indicators.m
@@ -1,5 +1,9 @@
 function quality_indicators(modelType, freq)
-% Returns quality indicators HTQ, TC25, SARmaxTum
+% Returns quality indicators HTQ, TC25/50/75, SARmaxTum, PLDmean
+% The PLD must be saved in Results>P_and_unscaled_settings
+% and be called P_modelType_freqMHz.mat
+% modelType is a string eg. 'duke_nasal', 'duke_tongue_salt', 'child'
+% freq is a number or a vector
 
 % Get paths
 filename = which('quality_indicators');
@@ -10,18 +14,14 @@ ppath = [resultpath filesep 'P_and_unscaled_settings'];
 addpath(resultpath, ppath)
 
 % Load matrices
-if nargin == 2
-    if length(freq)==1
-PLD = Yggdrasil.Utils.load([resultpath filesep 'P_and_unscaled_settings' filesep 'P_' modelType '_' num2str(freq) ...
-    'MHz.mat']);
-    elseif length(freq)>1
-        PLD = Yggdrasil.Utils.load([resultpath filesep 'P_and_unscaled_settings' filesep 'P_' modelType '_1_' num2str(freq(1)) ...
-    '_2_' num2str(freq(2)) 'MHz.mat']);
-    end
-% else
-%     [freq_comb_filename]= find_freq_comb(modelType, freq, 'P');
-%     PLD = Yggdrasil.Utils.load([resultpath filesep 'P_and_unscaled_settings' filesep freq_comb_filename]);
+if length(freq)==1
+    PLD = Yggdrasil.Utils.load([resultpath filesep 'P_and_unscaled_settings' filesep 'P_' modelType '_' num2str(freq) ...
+        'MHz.mat']);
+elseif length(freq)>1
+    freqstr = num2str(freq);
+    PLD = Yggdrasil.Utils.load([resultpath filesep 'P_and_unscaled_settings' filesep 'P_' modelType '_' regexprep(freqstr,'[^\w'']','') 'MHz.mat']);
 end
+
 tissue_mat = Yggdrasil.Utils.load([datapath filesep 'tissue_mat_' modelType '.mat']);
 
 % Get Q.I.
